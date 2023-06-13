1b4f54601edbaeeaa223f7c079cc841f400d5a65
BillieOdelle <billie@bogore.com>
2023-06-13 22:40:15 +1000

    committing further work on team project 3

--- AFL_SQLDB.sql
+++ AFL_SQLDB.sql
@@ -3,17 +3,17 @@ drop table if exists players CASCADE;
 drop table if exists games CASCADE;
 drop table if exists stats CASCADE;
 
--- CREATE table players
--- (
--- 	playerId INT NOT NULL,
--- 	displayName VARCHAR (200) NOT NULL,
--- 	height INT NOT NULL,
--- 	weight INT NOT NULL,
--- 	dob date NOT NULL
--- --	position VARCHAR NOT NULL,
--- --	origin VARCHAR (200) NOT NULL,
--- 	primary key (playerId)
--- );
+CREATE table players
+(
+	playerId DOUBLE PRECISION NOT NULL,
+	name VARCHAR (200) NOT NULL,
+	height INT NOT NULL,
+	weight INT NOT NULL,
+	dob date,
+	position VARCHAR,
+	origin VARCHAR,
+	primary key (playerId)
+);
 
 CREATE table games
 (
@@ -23,48 +23,48 @@ CREATE table games
 	date date NOT NULL,
 	venue VARCHAR (200) NOT NULL,
 	startTime TIME NOT NULL,
-	attendance INT NOT NULL,
+	attendance INT,
 	homeTeam VARCHAR (200) NOT NULL,
-	homeTeamsScore INT NOT NULL, 
+	homeTeamsScore INT, 
 	awayTeam VARCHAR (200) NOT NULL,
-	awayTeamScore INT NOT NULL,
-	rainfall DOUBLE PRECISION NOT NULL,
+	awayTeamScore INT,
+	rainfall DOUBLE PRECISION,
 	primary key (gameId)
 );
 
 CREATE table stats
 (
-gameId VARCHAR (200) NOT NULL,
-team VARCHAR (200) NOT NULL,
-year INT NOT NULL,
-round VARCHAR (200) NOT NULL,
-playerId INT NOT NULL,
-gameNumber INT NOT NULL,
-Disposal INT NOT NULL,
-Kicks INT NOT NULL,
-Marks INT NOT NULL,
-Handballs INT NOT NULL,
-Goals INT NOT NULL,
-Behinds INT NOT NULL,
--- Hit Outs INT NOT NULL,
-Tackles INT NOT NULL,
-Rebounds INT NOT NULL,
---Inside 50 INT NOT NULL,
-Clearances INT NOT NULL,
-Clangers INT NOT NULL,
-Frees INT NOT NULL,
---Frees Against INT NOT NULL,
---Brownlow Votes INT NOT NULL,
---Contested Possessions INT NOT NULL,
---Uncontested Possessions INT NOT NULL,
---Contested Marks INT NOT NULL,
---Marks Inside 50 INT NOT NULL,
---One Percenters INT NOT NULL,
-Bounces INT NOT NULL,
---Goal Assists INT NOT NULL,
---% Played INT NOT NULL,
-Subs  INT NOT NULL,
-primary key (year)
+	gameId VARCHAR (200) NOT NULL,
+	team VARCHAR (200) NOT NULL,
+	year INT NOT NULL,
+	round VARCHAR (200) NOT NULL,
+	playerId DOUBLE PRECISION NOT NULL,
+	name VARCHAR (200) NOT NULL,
+	gameNumber INT NOT NULL,
+	Disposal INT,
+	Kicks INT,
+	Marks INT,
+	Handballs INT,
+	Goals INT,
+	Behinds INT,
+	Hit_Outs INT,
+	Tackles INT,
+	Rebounds INT,
+	Inside_50 INT,
+	Clearances INT,
+	Clangers INT,
+	Frees INT,
+	Frees_Against INT,
+	Brownlow_Votes INT,
+	Contested_Possessions INT,
+	Uncontested_Possessions INT,
+	Contested_Marks INT,
+	Marks_Inside_50 INT,
+	One_Percenters INT,
+	Bounces INT,
+	Goal_Assists INT,
+	percent_Played INT,
+	Subs VARCHAR
 );
 
 
